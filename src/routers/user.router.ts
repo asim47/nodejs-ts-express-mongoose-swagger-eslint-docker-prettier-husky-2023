10b20bb8f6b30a683bdd31de46c289d0d5da27be
asim bilal <asimbilal@Asims-mac.local>
2023-03-16 11:04:29 +0500

    Lint

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,21 +1,21 @@
-  import * as express from 'express';
-  import { Request, Response, NextFunction } from 'express';
-  import * as UserController from '../controllers/user.controller';
+import * as express from 'express';
+import { Request, Response, NextFunction } from 'express';
+import * as UserController from '../controllers/user.controller';
 
-  class User {
-    public router: express.Router;
+class User {
+  public router: express.Router;
 
-    constructor() {
-      this.router = express.Router();
-      this.UserRouter();
-    }
-
-    private UserRouter(): void {
-      // @Route POST /user/register
-      // @DESC Register a user
-      // @ACCESS PUBLIC
-      this.router.post('/register', UserController.registerAdmin);
-    }
+  constructor() {
+    this.router = express.Router();
+    this.UserRouter();
   }
 
-  export const UserApi = new User().router;
+  private UserRouter(): void {
+    // @Route POST /user/register
+    // @DESC Register a user
+    // @ACCESS PUBLIC
+    this.router.post('/register', UserController.registerAdmin);
+  }
+}
+
+export const UserApi = new User().router;
